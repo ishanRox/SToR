9569ef57662f3284788e070ba5212588680ee631
Drone <roxishan8@gmail.com>
2021-07-06 11:13:51 +0530

    dynamic /:element close regex added

--- parsers/regexParser.js
+++ parsers/regexParser.js
@@ -1,12 +1,12 @@
 function regexParser(xmlDoc) {
     return xmlDoc
-      .match(/(.*xs:element.*)/g)
+      .match(/(.*:element.*)/g)
       .map((e) => e.trim())
       .reduce(
         (accum, val) => {
           const endSubArray = accum[accum.length - 1];
   
-          if (!endSubArray || val.includes(`</xs:element>`)) {
+          if (!endSubArray || /<\/.+:element>/.test(val)) {
             accum.push([]);
           } else {
             accum[accum.length - 1].push(val);
